824f42cf7ed3d91d527d12dde639b45f40debf55
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-18 11:21:05 +0530

    Commented Hardcoded Users Approach for RoleBasedAuthentication

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/yash/dev/cruddemo/security/DemoSecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/yash/dev/cruddemo/security/DemoSecurityConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -18,28 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,mary,susan);
-    }
 
     @Bean
     public SecurityFilterChain  filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -61,4 +36,28 @@ public class DemoSecurityConfig {
 
         return httpSecurity.build();
     }
+
+    //Commented java hardcoded user details
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,mary,susan);
+//    }
 }
